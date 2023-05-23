e24ad4d3b3017ca8dab737d9be340d8fd5c4260e
radar roark <122068506+radarroark@users.noreply.github.com>
2023-05-22 20:14:29 -0400

    compress/decompress incrementally

--- src/compress.zig
+++ src/compress.zig
@@ -1,33 +1,15 @@
 //! well, this isn't very good so far, but it's not like you
-//! could do any better. right now i'm loading the file
-//! completely in memory to compress/decompress. i set a
-//! really low file size limit and throw an error if it's too big.
-//! this will obviously have to change but i don't feel like
-//! figuring out incremental file compression right now, and
-//! i'd rather make progress elsewhere since i'll be testing with
-//! tiny files for a long time anyway.
+//! could do any better.
 
 const std = @import("std");
 const deflate = std.compress.deflate;
 const zlib = std.compress.zlib;
 
-const MAX_FILE_SIZE_BYTES = 1024;
-
-const CompressError = error{
-    FileTooLarge,
-};
+const MAX_FILE_READ_BYTES = 1024;
 
 /// zig doesn't have a zlib compressor in the stdlib yet, so this one is implemented
 /// manually. once it arrives, this code is getting tossed to the curb.
 pub fn compress(in: std.fs.File, out: std.fs.File, allocator: std.mem.Allocator) !void {
-    // read the in file into memory
-    var read_buffer = [_]u8{0} ** MAX_FILE_SIZE_BYTES;
-    const in_size = try in.pread(&read_buffer, 0);
-    if (in_size == MAX_FILE_SIZE_BYTES) {
-        return CompressError.FileTooLarge;
-    }
-    const read_slice = read_buffer[0..in_size];
-
     // define compression level
     const Level = enum(u2) {
         fastest = 0,
@@ -60,39 +42,52 @@ pub fn compress(in: std.fs.File, out: std.fs.File, allocator: std.mem.Allocator)
     try writer.writeAll(&.{ CMF, FLG });
 
     // do the dirty work
-    const comp_size = try comp.write(read_slice);
-    try comp.close();
-
-    // write the hash
+    try in.seekTo(0);
+    const reader = in.reader();
     var hasher = std.hash.Adler32.init();
-    hasher.update(read_slice[0..comp_size]);
+    var buf = [_]u8{0} ** MAX_FILE_READ_BYTES;
+    while (true) {
+        // read from file
+        const size = try reader.read(&buf);
+        if (size == 0) break;
+        // update hash
+        hasher.update(buf[0..size]);
+        // compress
+        _ = try comp.write(buf[0..size]);
+    }
+    try comp.close();
+    // write the hash
     try writer.writeIntBig(u32, hasher.final());
 }
 
 pub fn decompress(allocator: std.mem.Allocator, in: std.fs.File, out: std.fs.File, skip_header: bool) !void {
-    // read the in file into memory
-    var read_buffer = [_]u8{0} ** MAX_FILE_SIZE_BYTES;
-    const in_size = try in.pread(&read_buffer, 0);
-    if (in_size == MAX_FILE_SIZE_BYTES) {
-        return CompressError.FileTooLarge;
-    }
-    var fixed_buffer = std.io.fixedBufferStream(read_buffer[0..in_size]);
-
-    // do the dirty work
-    var zlib_stream = try zlib.zlibStream(allocator, fixed_buffer.reader());
+    // init stream from input file
+    try in.seekTo(0);
+    var zlib_stream = try zlib.zlibStream(allocator, in.reader());
     defer zlib_stream.deinit();
+    const reader = zlib_stream.reader();
     if (skip_header) {
-        try zlib_stream.reader().skipUntilDelimiterOrEof(0);
+        try reader.skipUntilDelimiterOrEof(0);
     }
-    const buf = try zlib_stream.reader().readAllAlloc(allocator, std.math.maxInt(usize));
-    defer allocator.free(buf);
 
-    // write the result to the out file
-    _ = try out.write(buf);
+    // write the decompressed data to the output file
+    const writer = out.writer();
+    var buf = [_]u8{0} ** MAX_FILE_READ_BYTES;
+    while (true) {
+        // read from file
+        const size = try reader.read(&buf);
+        if (size == 0) break;
+        // decompress
+        _ = try writer.write(buf[0..size]);
+    }
 }
 
 // does the same thing as decompress, except it remains completely
 // in memory and can clean itself up via deinit.
+const MAX_FILE_SIZE_BYTES = 1024;
+const CompressError = error{
+    FileTooLarge,
+};
 pub const Decompressed = struct {
     stream: std.compress.zlib.ZlibStream(std.io.Reader(*std.io.FixedBufferStream([]u8), std.io.FixedBufferStream([]u8).ReadError, std.io.FixedBufferStream([]u8).read)),
 
