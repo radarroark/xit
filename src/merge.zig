6852d309b100de524e337c4530e33d56bb169616
radar roark <122068506+radarroark@users.noreply.github.com>
2024-05-31 11:53:16 -0400

    rename target to source in merge fn

--- src/merge.zig
+++ src/merge.zig
@@ -7,16 +7,16 @@ const chk = @import("./checkout.zig");
 const io = @import("./io.zig");
 const rp = @import("./repo.zig");
 
-pub fn merge(comptime repo_kind: rp.RepoKind, core: *rp.Repo(repo_kind).Core, allocator: std.mem.Allocator, target: []const u8) ![hash.SHA1_HEX_LEN]u8 {
-    // get the current oid, target oid, and common oid
+pub fn merge(comptime repo_kind: rp.RepoKind, core: *rp.Repo(repo_kind).Core, allocator: std.mem.Allocator, source: []const u8) ![hash.SHA1_HEX_LEN]u8 {
+    // get the current oid, source oid, and common oid
     const current_oid = try ref.readHead(repo_kind, core);
-    const target_oid = try ref.resolve(repo_kind, core, target) orelse return error.InvalidTarget;
-    const common_oid = try obj.commonAncestor(repo_kind, allocator, core, &current_oid, &target_oid);
+    const source_oid = try ref.resolve(repo_kind, core, source) orelse return error.InvalidTarget;
+    const common_oid = try obj.commonAncestor(repo_kind, allocator, core, &current_oid, &source_oid);
 
     // compare the commits
     var tree_diff = obj.TreeDiff(repo_kind).init(allocator);
     defer tree_diff.deinit();
-    try tree_diff.compare(core, common_oid, target_oid, null);
+    try tree_diff.compare(core, common_oid, source_oid, null);
 
     switch (repo_kind) {
         .git => {
@@ -51,11 +51,11 @@ pub fn merge(comptime repo_kind: rp.RepoKind, core: *rp.Repo(repo_kind).Core, al
     }
 
     // create commit message
-    const commit_message = try std.fmt.allocPrint(allocator, "merge from {s}", .{target});
+    const commit_message = try std.fmt.allocPrint(allocator, "merge from {s}", .{source});
     defer allocator.free(commit_message);
 
     // commit the change
-    const parent_oids = &[_][hash.SHA1_HEX_LEN]u8{ current_oid, target_oid };
+    const parent_oids = &[_][hash.SHA1_HEX_LEN]u8{ current_oid, source_oid };
     var sha1_bytes_buffer = [_]u8{0} ** hash.SHA1_BYTES_LEN;
     try obj.writeCommit(repo_kind, core, allocator, parent_oids, commit_message, &sha1_bytes_buffer);
 
