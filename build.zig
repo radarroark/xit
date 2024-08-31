bd1a70dcb3332faa81c4fedfeb160a5cec140ac3
radar roark <122068506+radarroark@users.noreply.github.com>
2024-08-31 12:27:04 -0400

    start work on testnet

--- build.zig
+++ build.zig
@@ -57,7 +57,7 @@ pub fn build(b: *std.Build) !void {
         run_step.dependOn(&run_cmd.step);
     }
 
-    // unit tests
+    // test
     {
         const zlib = @import("deps/test/zlib.zig");
         const mbedtls = @import("deps/test/mbedtls.zig");
@@ -87,6 +87,39 @@ pub fn build(b: *std.Build) !void {
         const run_unit_tests = b.addRunArtifact(unit_tests);
         run_unit_tests.has_side_effects = true;
         const test_step = b.step("test", "Run unit tests");
+        test_step.dependOn(&run_unit_tests.step);
+    }
+
+    // testnet
+    {
+        const zlib = @import("deps/test/zlib.zig");
+        const mbedtls = @import("deps/test/mbedtls.zig");
+        const libgit2 = @import("deps/test/libgit2.zig");
+
+        const z = zlib.create(b, target, optimize);
+        const tls = mbedtls.create(b, target, optimize);
+
+        const git2 = try libgit2.create(b, target, optimize);
+        tls.link(git2.step);
+        z.link(git2.step);
+
+        const unit_tests = b.addTest(.{
+            .root_source_file = b.path("src/testnet.zig"),
+            .optimize = optimize,
+        });
+        unit_tests.root_module.addAnonymousImport("xitdb", .{
+            .root_source_file = b.path("../xitdb/src/lib.zig"),
+        });
+        unit_tests.root_module.addAnonymousImport("xitui", .{
+            .root_source_file = b.path("../xitui/src/lib.zig"),
+        });
+        unit_tests.linkLibC();
+        unit_tests.addIncludePath(b.path("deps/test/libgit2/include"));
+        unit_tests.linkLibrary(git2.step);
+
+        const run_unit_tests = b.addRunArtifact(unit_tests);
+        run_unit_tests.has_side_effects = true;
+        const test_step = b.step("testnet", "Run network unit tests");
         test_step.dependOn(&run_unit_tests.step);
     }
 }
