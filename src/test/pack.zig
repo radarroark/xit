4b6fc169d263ac8de2961f759c737f00863d5558
radar roark <122068506+radarroark@users.noreply.github.com>
2024-09-23 09:53:25 -0400

    write one byte at a time in test

--- src/test/pack.zig
+++ src/test/pack.zig
@@ -221,7 +221,7 @@ test "pack" {
         var pack_file = try temp_dir.createFile("test.pack", .{});
         defer pack_file.close();
 
-        var buffer = [_]u8{0} ** 1024;
+        var buffer = [_]u8{0} ** 1;
         while (true) {
             const size = try pack_writer.read(&buffer);
             try pack_file.writeAll(buffer[0..size]);
