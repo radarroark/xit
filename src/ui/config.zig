0d11e8ad2fb1a12067efd31ac5b19f8e6a8cdbe0
radar roark <122068506+radarroark@users.noreply.github.com>
2024-09-25 14:35:32 -0400

    make text fields use dashed border style

--- src/ui/config.zig
+++ src/ui/config.zig
@@ -41,9 +41,9 @@ pub fn ConfigItem(comptime Widget: type) type {
             self.clearGrid();
             for (self.box.children.keys(), self.box.children.values()) |id, *item| {
                 item.widget.text_box.border_style = if (self.getFocus().child_id == id)
-                    (if (root_focus.grandchild_id == id) .double else .hidden)
+                    (if (root_focus.grandchild_id == id) .double else .dashed)
                 else
-                    .hidden;
+                    .dashed;
             }
             try self.box.build(constraint, root_focus);
         }
