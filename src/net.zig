8119f0ddd3456f4c4a9e95ede572649513a83164
radar roark <122068506+radarroark@users.noreply.github.com>
2024-10-02 10:20:25 -0400

    start reading messages from git-upload-pack

--- src/net.zig
+++ src/net.zig
@@ -11,6 +11,7 @@ pub fn fetch(allocator: std.mem.Allocator, host: []const u8, port: u16, repo: []
     defer sock.close();
     try sock.setTimeouts(TIMEOUT_MICRO_SECS, TIMEOUT_MICRO_SECS);
 
+    const reader = sock.reader();
     const writer = sock.writer();
 
     // send command
@@ -24,5 +25,66 @@ pub fn fetch(allocator: std.mem.Allocator, host: []const u8, port: u16, repo: []
         @memcpy(command[command_size_buf.len - command_size.len .. command_size_buf.len], command_size);
 
         try writer.writeAll(command);
+    }
+
+    var arena = std.heap.ArenaAllocator.init(allocator);
+    defer arena.deinit();
+
+    var ref_to_hash = std.StringArrayHashMap([]const u8).init(arena.allocator());
+    var capabilities = std.ArrayList([]const u8).init(arena.allocator());
+
+    // read messages
+    while (true) {
+        var size_buffer = [_]u8{0} ** 4;
+        try reader.readNoEof(&size_buffer);
+
+        var msg_size: usize = try std.fmt.parseInt(u16, &size_buffer, 16);
+
+        // a flush packet has a size of 0
+        if (msg_size == 0) {
+            break;
+        }
+
+        // subtract the size of the hexadecimal size itself
+        // to get the rest of the message size
+        if (msg_size <= size_buffer.len) {
+            return error.UnexpectedMessageSize;
+        }
+        msg_size -= size_buffer.len;
+
+        // alloc the buffer that will hold the message
+        var msg_buffer = try arena.allocator().alloc(u8, msg_size);
+        try reader.readNoEof(msg_buffer);
+
+        // ignore newline char
+        if (msg_buffer[msg_buffer.len - 1] == '\n') {
+            msg_buffer = msg_buffer[0 .. msg_buffer.len - 1];
+        } else {
+            return error.NewlineByteNotFound;
+        }
+
+        // the very first message should have a null byte
+        // and include the capabilities after
+        if (std.mem.indexOfScalar(u8, msg_buffer, 0)) |null_index| {
+            if (ref_to_hash.count() == 0) {
+                var iter = std.mem.splitScalar(u8, msg_buffer[null_index + 1 ..], ' ');
+                while (iter.next()) |cap| {
+                    try capabilities.append(cap);
+                }
+
+                msg_buffer = msg_buffer[0..null_index];
+            } else {
+                return error.UnexpectedNullByte;
+            }
+        }
+
+        // populate the map of refs
+        if (std.mem.indexOfScalar(u8, msg_buffer, ' ')) |space_index| {
+            try ref_to_hash.put(msg_buffer[space_index + 1 ..], msg_buffer[0..space_index]);
+        }
+    }
+
+    for (ref_to_hash.keys(), ref_to_hash.values()) |ref, hash| {
+        std.debug.print("{s} {s}\n", .{ ref, hash });
     }
 }
