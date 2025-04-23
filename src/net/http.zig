radarroark/xit

commit 92c5c2820e8867ac90ce1d36a8dd7d9afe5b474c

support default http proxy from environment

src/net/http.zig
@@ -8,8 +8,11 @@ pub const HttpState = struct {
     sent_write_request: bool,
 
     pub fn init(allocator: std.mem.Allocator) !HttpState {
+        var client: std.http.Client = .{ .allocator = allocator };
+        try client.initDefaultProxies(allocator);
+
         return .{
-            .http_client = std.http.Client{ .allocator = allocator },
+            .http_client = client,
             .read_request = null,
             .write_request = null,
             .sent_write_request = false,
@@ -17,6 +20,9 @@ pub const HttpState = struct {
     }
 
     pub fn deinit(self: *HttpState) void {
+        if (self.http_client.http_proxy) |proxy| self.http_client.allocator.destroy(proxy);
+        if (self.http_client.https_proxy) |proxy| self.http_client.allocator.destroy(proxy);
+
         self.http_client.deinit();
         close(self);
     }

commit 44bd59c6e17010f9612d95bd00609f8217de1c46

use arena in HttpState
Unfornately, 92c5c282 introduced the following memory leaks:

```
/home/tw/data/code/zig_stage4/stage3/lib/zig/std/mem/Allocator.zig:423:40: 0x1674497 in dupe__anon_8530 (xit)
    const new_buf = try allocator.alloc(T, m.len);
                                       ^
/home/tw/data/code/zig_stage4/stage3/lib/zig/std/process.zig:418:30: 0x17e0899 in getEnvVarOwned (xit)
        return allocator.dupe(u8, result);
                             ^
/home/tw/data/code/zig_stage4/stage3/lib/zig/std/http/Client.zig:1244:51: 0x17e0133 in createProxyFromEnvVar (xit)
        const content = std.process.getEnvVarOwned(arena, name) catch |err| switch (err) {
                                                  ^
/home/tw/data/code/zig_stage4/stage3/lib/zig/std/http/Client.zig:1230:54: 0x17e27d5 in initDefaultProxies (xit)
        client.http_proxy = try createProxyFromEnvVar(arena, &.{
                                                     ^
/home/tw/data/code/xit/src/net/http.zig:12:38: 0x17e2985 in init (xit)
        try client.initDefaultProxies(allocator);
                                     ^
/home/tw/data/code/xit/src/net/wire.zig:167:64: 0x17e362e in init (xit)
                .http => .{ .http = try net_http.HttpState.init(allocator) },
                                                               ^

error(gpa): memory address 0x7f4e75ee0120 leaked:
/home/tw/data/code/zig_stage4/stage3/lib/zig/std/mem/Allocator.zig:423:40: 0x1674497 in dupe__anon_8530 (xit)
    const new_buf = try allocator.alloc(T, m.len);
                                       ^
/home/tw/data/code/zig_stage4/stage3/lib/zig/std/process.zig:418:30: 0x17e0899 in getEnvVarOwned (xit)
        return allocator.dupe(u8, result);
                             ^
/home/tw/data/code/zig_stage4/stage3/lib/zig/std/http/Client.zig:1244:51: 0x17e0133 in createProxyFromEnvVar (xit)
        const content = std.process.getEnvVarOwned(arena, name) catch |err| switch (err) {

src/net/http.zig
@@ -6,25 +6,31 @@ pub const HttpState = struct {
     read_request: ?std.http.Client.Request,
     write_request: ?std.http.Client.Request,
     sent_write_request: bool,
+    arena: *std.heap.ArenaAllocator,
+    allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator) !HttpState {
-        var client: std.http.Client = .{ .allocator = allocator };
-        try client.initDefaultProxies(allocator);
+        var arena = try allocator.create(std.heap.ArenaAllocator);
+        arena.* = .init(allocator);
+        var client: std.http.Client = .{ .allocator = arena.allocator() };
+        try client.initDefaultProxies(arena.allocator());
 
         return .{
             .http_client = client,
             .read_request = null,
             .write_request = null,
             .sent_write_request = false,
+            .arena = arena,
+            .allocator = allocator,
         };
     }
 
     pub fn deinit(self: *HttpState) void {
-        if (self.http_client.http_proxy) |proxy| self.http_client.allocator.destroy(proxy);
-        if (self.http_client.https_proxy) |proxy| self.http_client.allocator.destroy(proxy);
-
         self.http_client.deinit();
         close(self);
+        self.arena.deinit();
+        self.allocator.destroy(self.arena);
+        self.* = undefined;
     }
 
     pub fn close(self: *HttpState) void {